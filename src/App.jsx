84fdec3966eefe9238ad676abbcc197c68bd23d0
Doni J <donijosy67@gmail.com>
2025-10-07 11:56:30 +0300

    Random nebers generating

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import Die from "./Die"
+import Die from "./Die";
+import { use, useState } from "react";
 function App() {
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  const elements = dice.map((numbers) => <Die value={numbers} />);
+
   return (
     <main>
-      <div className="dice-container">
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-        <Die value= {1}/>
-      </div>
+      <div className="dice-container">{elements}</div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
